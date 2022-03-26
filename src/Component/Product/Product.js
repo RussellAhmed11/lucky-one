e45b1c71b8db6a7885879218a78d3997d600df2e
Rasel Mia <ahmedrussell437@gmail.com>
2022-03-26 11:22:23 +0600

    cart degin done

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -7,7 +7,13 @@ const Product = (props) => {
     return (
         <div className='product'>
            <img src={img} alt="" /> 
+           <div className='product-info'>
            <p className='product_name'>Name:{name}</p>
+           <p>Price:{price}</p>
+           <p><small>Seller:{seller}</small></p>
+           <p><small>Ratting:{ratings} Starts</small></p>
+           </div>
+           <button className='btn-cart'><p className='btn-text'>Add to cart</p></button>
         </div>
     );
 };
