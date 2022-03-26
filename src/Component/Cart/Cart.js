38e3bd3b75e39e3d89ec84bb69d464d6d799846e
Rasel Mia <ahmedrussell437@gmail.com>
2022-03-26 16:41:04 +0600

    edit redmi file

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -2,19 +2,16 @@ import React from 'react';
 import SelectedCart from '../SelctedCart/SelectedCart';
 import './Cart.css'
 const Cart = ({carts}) => {
-    console.log(carts)
-    return (
-       
-        <div className='cart'>
-            <h3>Order Summary</h3>
-            {
-                carts.map(cart=> <SelectedCart cart={cart} key={cart.id}></SelectedCart>)
-            }
-            <button className='btn-1'>Chose 1 for me</button><br />
-            <button className='btn-2'>Chose Again</button>
-        </div>
-    );
+        return (     
+            <div className='cart'>
+                <h3>Order Summary</h3>
+                {
+                    carts.map(cart=> <SelectedCart cart={cart} key={cart.id}></SelectedCart>)
+                }
+                <button className='btn-1'>Chose 1 for me</button><br />
+                <button className='btn-2'>Chose Again</button>
+            </div>
+        );    
 };
-
 export default Cart;
 
--- src/Component/SelctedCart/SelectedCart.js
+++ src/Component/SelctedCart/SelectedCart.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './SelectedCart.css'
 
 const SelectedCart = ({cart}) => {
-    const {img,name,price}=cart
-    return (
-        <div>
-        <div className='selected-cart'>
-            <img src={img} alt="" />
-            <p>{name}</p>
-            <p>${price}</p>
-        </div>
-        </div>
-    );
+    const {img,name,price}=cart;
+        return (
+            <div>
+                <div className='selected-cart'>
+                    <img src={img} alt="" />
+                    <p>{name}</p>
+                    <p>${price}</p>
+                </div>
+            </div>
+        );
 };
 
 export default SelectedCart;
